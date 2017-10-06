4c4754ca061f2e6beba78a6722fb79543de678ec
pedrodelapena <pedrodelapena@uol.com.br>
2017-10-06 17:04:00 -0300

    db no encoder

--- encoder.py
+++ encoder.py
@@ -21,13 +21,28 @@ def generateWave(putin,duration):
     for i in range(0,len(sound)):
         sound[i] = np.sin(2*math.pi  * key[0] * (i/fs)) + np.sin(2*math.pi  * key[1] * (i/fs))
     return sound
+	
+def DB(energy):
+    convert = []
+    for i in energy:
+        db = 10*math.log10(energy[i]/20000)
+        convert.append(db)
+    return convert
+
+plt.title('Transmitido')	
+plt.xlabel('Frequencia (Hz)', fontsize=18)
+plt.ylabel('Decibel(dB)', fontsize=18)
 
 while(True):
-    print('Digite um numero :')
-    key = int(input())
-    beep = generateWave(key,1)
-    playSound(beep)
-    plt.plot(np.arange(1000),beep[:1000])
-    plt.show()
-    plt.plot(np.arange(2300),np.abs(fft(beep))[:2300])
-    plt.show()
+	print('Digite um numero :')
+	key = int(input())
+	beep = generateWave(key,1)
+	playSound(beep)
+	plt.plot(np.arange(1000),beep[:1000])
+	plt.show()
+	plt.plot(np.arange(2300),DB(np.abs(fft(beep)))[:2300])
+	plt.title('Transmitido', fontsize=18)
+	plt.xlabel('Frequencia (Hz)', fontsize=16)
+	plt.ylabel('Decibel(dB)', fontsize=16)
+	plt.show()
+
